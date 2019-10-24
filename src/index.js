30791ea2c3c6e5ba42bcd6a8986174cbfadc00e2
Omar Jadan <o.jadan197@gmail.com>
2019-10-24 16:25:14 -0400

    some bet stuff is functional

--- src/index.js
+++ src/index.js
@@ -1,31 +1,54 @@
 const {
-    default: { singleDeckGame }
-  } = require("blackjack-dealer-logic");
+  default: { singleDeckGame }
+} = require("blackjack-dealer-logic");
 
-    const Dom = require("./utils/Dom");
+const Dom = require("./utils/Dom");
 
-    const playerCard = document.querySelector(".game__player-cards");
-    const dealerCard = document.querySelector(".game__dealer-cards");
+const playerCard = document.querySelector(".game__player-cards");
+const dealerCard = document.querySelector(".game__dealer-cards");
+singleDeckGame.deal();
 
-    singleDeckGame.deal();
-    
-    const userHand = singleDeckGame.getUserHand();
-    const dealerHand = singleDeckGame.getDealerHand(); 
+const userHand = singleDeckGame.getUserHand();
+const dealerHand = singleDeckGame.getDealerHand();
 
-    Dom.displayCards(userHand.getCards(), playerCard);
-    Dom.displayCards(dealerHand.getCards(), dealerCard);
+Dom.displayCards(userHand.getCards(), playerCard);
+Dom.displayCards(dealerHand.getCards(), dealerCard);
 
-    const hit_button = document.querySelector(".action__hit");
-    hit_button.addEventListener("click", () => {
-        singleDeckGame.evaluateUser();
-        
-        if (!singleDeckGame.isUserBust()){
-            singleDeckGame.hitUser();
-            playerCard.innerHTML = "";
-            Dom.displayCards(userHand.getCards(), playerCard);
-        }
-})
+console.log(singleDeckGame.getAnte());
+console.log(singleDeckGame.getUserChips());
 
+let userChips = document.querySelector(".user__chips");
+userChips.textContent = singleDeckGame.getUserChips();
 
+let currentWager = document.querySelector(".user__wager");
+currentWager.textContent = singleDeckGame.getAnte();
+
+let wagerInput = document.querySelector(".wager__input");
+// wagerInput.min = singleDeckGame.getAnte();
+
+const hit_button = document.querySelector(".action__hit");
+hit_button.addEventListener("click", () => {
+  Dom.clickHit(singleDeckGame);
+  playerCard.innerHTML = ""; 
+  Dom.displayCards(userHand.getCards(), playerCard);
+});
+
+const stand_button = document.querySelector(".action__stand");
+stand_button.addEventListener("click", () => {
+  Dom.clickStand(singleDeckGame);
+  playerCard.innerHTML = ""; 
+  Dom.displayCards(userHand.getCards(), playerCard);
+});
+
+const wager_button = document.querySelector(".action__wager");
+wager_button.addEventListener("click", (() => {
+  let wager = wagerInput.value;
+  Dom.clickWager(singleDeckGame, wager);
+  currentWager.textContent = singleDeckGame.getAnte();
+  userChips.textContent = singleDeckGame.getUserChips();
+  wagerInput.hidden = true;
+  wager_button.hidden = true;
+  
+}));
 
 
--- src/utils/Dom.js
+++ src/utils/Dom.js
@@ -3,31 +3,56 @@ module.exports = {
 
         const playing_card = document.createElement("div");
         playing_card.classList.add("playing-card");
-    
+
         const container = document.createElement("div");
         container.classList.add("playing-card__container");
         container.classList.add("red-suit");
-    
+
         const suit = document.createElement("div");
         suit.classList.add("playing-card__suit");
         suit.textContent = card.getSuit();
-    
+
         const value = document.createElement("div");
         value.classList.add("playing-card__value");
         value.textContent = card.getValue();
-    
+
         container.append(suit);
         container.append(value);
         playing_card.append(container);
-    
+
         return playing_card;
 
+    },
+
+    disableActionButtons() {
+        const actionsContainer = document.querySelector(".game__player-actions");
+        const actionButtons = actionsContainer.querySelectorAll(".button");
+        actionButtons.forEach(button => button.setAttribute("disabled", "true"));
     },
 
     displayCards(cardsArray, containerElement) {
         cardsArray.forEach(card => {
             containerElement.append(this.createPlayingCard(card));
         });
+    },
+
+    clickStand(singleDeckGame) {
+        singleDeckGame.standUser();
+        singleDeckGame.evaluateUser();
+        this.disableActionButtons();
+    },
+
+    clickHit(singleDeckGame) {
+        singleDeckGame.evaluateUser();
+        if (!singleDeckGame.isUserBust()) {
+            singleDeckGame.hitUser();
+        }
+    },
+
+    clickWager(singleDeckGame, wager) {
+        singleDeckGame.resetAnte();
+        singleDeckGame.receiveAnte(wager);
     }
+
 
 };
